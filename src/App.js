c96c6be7f88204d57a682da8b1767dbbf6023394
Divesh Jadhav <diveshvjadhav@gmail.com>
2023-05-05 20:19:47 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import React from "react";
 import StudentForm from "./Components/Registration/StudentForm";
-import InstituteHomePage from "./Components/InstitutionPages/InstituteHomePage"
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Verifier from "./Components/VerifierPage/Verifier";
-import UploadCertificate from "./Components/InstitutionPages/UploadCertificate";
-import ListOfCertificate from "./Components/InstitutionPages/ListOfCertificate";
 import DownloadCertificate from "./Components/DownloadCertificate/DownloadCertificate";
-import SendCertificate from "./Components/InstitutionPages/SendCertificate";
 import HomePage from "./Components/LandingPage/HomePage";
 
 const App = () => {
@@ -17,10 +13,6 @@ const App = () => {
           <Route path="/" element={<HomePage/>}/>
           <Route path="/studentform" element={<StudentForm/>}/>
           <Route path="/verifier" element={<Verifier/>}/>
-          <Route path="/institutehomepage" element={<InstituteHomePage />} />
-          <Route path="/Uploadcertificate" element={<UploadCertificate />} />
-          <Route path="/listofcertificate" element={<ListOfCertificate />} />
-          <Route path="/sendcertificate" element={<SendCertificate />} />
           <Route path="/downloadcertificate" element={<DownloadCertificate />} />
         </Routes>
       </div>
