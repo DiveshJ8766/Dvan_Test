d438cb65dd06b75ec7ffa97c7b06118b59ce0caa
Divesh Jadhav <diveshvjadhav@gmail.com>
2023-05-06 19:40:55 +0530

    Update Verifier.js

--- src/Components/VerifierPage/Verifier.js
+++ src/Components/VerifierPage/Verifier.js
@@ -57,7 +57,7 @@ return (
             }
           </div>
           <button type="submit"
-            className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Check</button>
+            className="text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Check</button>
           <Link to="/"
             className=" text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 ml-2">Go Back</Link>
         </form>
