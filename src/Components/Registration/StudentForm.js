261c805db280b0575d3796c6021bc1ec752df525
Divesh Jadhav <diveshvjadhav@gmail.com>
2023-05-06 19:40:17 +0530

    Update StudentForm.js

--- src/Components/Registration/StudentForm.js
+++ src/Components/Registration/StudentForm.js
@@ -140,8 +140,9 @@ return (
             </select>
           </div>
           <button type="submit"
-            className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Register
-            new account</button>
+            className="text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Register</button>
+          <Link to="/"
+            className=" text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 ml-3">Go Back</Link>
         </form>
       </div>
     </div>
